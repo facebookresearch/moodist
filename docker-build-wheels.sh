c7d6d4b0dffe2723cb3ae8bc8b5983c5344b8481
tscmoo <the.dengus@gmail.com>
2025-03-17 07:07:46 +0000

    Let's build for some pre-release versions of pytorch

--- docker-build-wheels.sh
+++ docker-build-wheels.sh
@@ -12,12 +12,20 @@ versions="cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313"
 
 torch_versions="2.5 2.6"
 
+pre_torch_versions="2.7.* 2.8.*"
+
 orig_path=$PATH
 for ver in $versions; do
     export PATH=/opt/python/$ver/bin:$orig_path
 
     for torchver in $torch_versions; do
         pip install torch==$torchver
+        python setup.py clean --all
+        python setup.py bdist_wheel -k --plat manylinux_2_28_x86_64
+    done
+
+    for torchver in $pre_torch_versions; do
+        pip install --pre --index-url https://download.pytorch.org/whl/nightly/cu128 torch==$torchver
         python setup.py clean --all
         python setup.py bdist_wheel -k --plat manylinux_2_28_x86_64
     done
